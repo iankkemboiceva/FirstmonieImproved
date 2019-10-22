1572ca400a21e1131118b4deab69bf14881f28af
Administrator <ian@cevaltd.com>
2019-10-22 09:25:14 +0100

    Commit with Kotlin Presenter

---
+++ app/src/main/java/firstmob/firstbank/com/firstagent/presenter/ActivateAgentPresenter.kt
@@ -0,0 +1,150 @@
+package firstmob.firstbank.com.firstagent.presenter
+
+import android.os.Handler
+import android.widget.Toast
+
+import org.json.JSONException
+import org.json.JSONObject
+
+import javax.inject.Inject
+
+import firstmob.firstbank.com.firstagent.Activity.ApplicationClass
+import firstmob.firstbank.com.firstagent.constants.Constants
+import firstmob.firstbank.com.firstagent.contract.ActivateAgentContract
+import firstmob.firstbank.com.firstagent.contract.MainContract
+
+import firstmob.firstbank.com.firstagent.security.SecurityLayer
+import firstmob.firstbank.com.firstagent.utils.Utility
+import firstmob.firstbank.com.firstagent.utils.Utility.*
+
+class ActivateAgentPresenter(internal var iLoginView: ActivateAgentContract.ILoginView?, private val getDataIntractor: MainContract.GetDataIntractor) : ActivateAgentContract.Presenter, MainContract.GetDataIntractor.OnFinishedListener {
+
+    @Inject
+    internal lateinit var ul: Utility
+
+
+    init {
+
+        ApplicationClass.getMyComponent().inject(this)
+        // initUser();
+    }
+
+
+    override fun DevReg(agpin: String, otp: String) {
+
+
+        //   ul.checkpermissions();
+
+        iLoginView!!.showProgress()
+
+        val endpoint = "reg/devReg.action/"
+        val ip = getIP()
+        val mac = getMacAddress()
+        val serial = getSerial()
+        val version = getDevVersion()
+        val devtype = getDevModel()
+        val imei = getDevImei()
+        if (checkInternetConnection()) {
+            if (isNotNull(imei) && isNotNull(serial)) {
+
+
+                //   final   String agid = agentid.getText().toString();
+                val agid = "PREF"
+
+                val regId = "sss"
+                val encrypted = Utility.b64_sha256(agpin)
+                SecurityLayer.Log("Encrypted Pin", encrypted)
+                val params = Constants.CH_ID + "/" + agid + "/" + otp + "/" + encrypted + "/secans1/" + "secans2" + "/secans3/" + mac + "/" + ip + "/" + imei + "/" + serial + "/" + version + "/" + devtype + "/" + regId
+
+                val urlparams = ul!!.firsttimelogin(params, endpoint)
+                getDataIntractor.getResults(this, urlparams)
+
+            } else {
+
+
+                iLoginView!!.showToast("Please ensure this device has an IMEI number")
+
+                iLoginView!!.hideProgress()
+
+            }
+        }
+
+    }
+
+
+    override fun onFinished(responsebody: String) {
+
+        try {
+            // JSON Object
+            SecurityLayer.Log("response..:", responsebody)
+
+
+            var obj = JSONObject(responsebody)
+            /*   JSONObject jsdatarsp = obj.optJSONObject("data");
+                    SecurityLayer.Log("JSdata resp", jsdatarsp.toString());
+                    //obj = Utility.onresp(obj,getActivity()); */
+            obj = SecurityLayer.decryptFirstTimeLogin(obj)
+            SecurityLayer.Log("decrypted_response", obj.toString())
+
+            val respcode = obj.optString("responseCode")
+            val responsemessage = obj.optString("message")
+
+
+            //session.setString(SecurityLayer.KEY_APP_ID,appid);
+
+            if (isNotNull(respcode) && isNotNull(responsemessage)) {
+
+                SecurityLayer.Log("Response Message", responsemessage)
+
+                if (respcode == "00") {
+                    val datas = obj.optJSONObject("data")
+
+
+                    iLoginView!!.showToast("SUCCESS")
+
+
+                } else {
+
+                    iLoginView!!.showToast(responsemessage)
+
+
+                }
+
+            } else {
+
+
+                iLoginView!!.showToast("There was an error on your request")
+
+
+            }
+
+        } catch (e: JSONException) {
+            SecurityLayer.Log("encryptionJSONException", e.toString())
+
+            iLoginView!!.showToast("There was an error on your request")
+            // SecurityLayer.Log(e.toString());
+
+        } catch (e: Exception) {
+            SecurityLayer.Log("encryptionJSONException", e.toString())
+            iLoginView!!.showToast("There was an error on your request")
+            // SecurityLayer.Log(e.toString());
+        }
+
+
+        //   iLoginView.onLoginResult(response);
+        iLoginView!!.hideProgress()
+    }
+
+    override fun onFailure(t: Throwable) {
+        iLoginView!!.hideProgress()
+        iLoginView!!.showToast("There was an error on your request")
+        //    iLoginView!!.onLoginError(t.toString())
+
+    }
+
+    override fun ondestroy() {
+        iLoginView = null
+    }
+
+
+}
